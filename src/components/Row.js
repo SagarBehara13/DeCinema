cb7822af6b5c85b9c4bf5af19ad70c3cb26284c9
Yash Agrawal <yashagrawal18@outlook.com>
2020-12-17 22:21:36 +0530

    Base working project

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,70 +1,104 @@
-import React, { useState, useEffect } from "react"
+import Web3 from 'web3'
+import React, { Component } from "react"
 import ScrollContainer from "react-indiana-drag-scroll"
+import { Spinner } from 'reactstrap'
 
 import "./Row.css"
-import axios from "./axios"
-import YouTube from "react-youtube"
-import movieTrailer from "movie-trailer"
+import Token from '../abis/Token.json'
 
 
-const base_url = "https://image.tmdb.org/t/p/original/"
+class Row extends Component {
 
+  constructor(props) {
+    super(props)
 
-function Row({ title, fetchUrl, isLargeRow }) {
-  const [movies, setMovies] = useState([])
-  const [trailerUrl, setTrailerUrl] = useState("")
-
-  useEffect(() => {
-    async function fetchData() {
-      const request = await axios.get(fetchUrl)
-      setMovies(request.data.results)
+    this.state = {
+      filmCount: 0,
+      film: null,
+      loading: true,
+      account: '',
+      filmDetails: []
     }
-    fetchData()
-  }, [fetchUrl])
-
-  const youtubeOpts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      autoplay: 1,
-    },
   }
 
-  const movieClicked = (moviename) => {
-    console.log(moviename)
-    if (trailerUrl !== "") setTrailerUrl("")
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
     else {
-      movieTrailer(moviename)
-        .then((url) => {
-          const urlParamV = new URLSearchParams(new URL(url).search)
-          setTrailerUrl(urlParamV.get("v"))
-        })
-        .catch((err) => console.log(err))
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   }
 
-  return (
-    <div className="row">
-      <h2>{title}</h2>
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
 
-      <ScrollContainer className="row__posters">
-        {movies.map((movie) => (
-          <img
-            onClick={() =>
-              movieClicked(movie.name || movie.title || movie.orginal_name)
-            }
-            key={movie.id}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`} //use && if theres no else or : otherwise use ?
-            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-            alt={movie.name}
-          />
-        ))}
-      </ScrollContainer>
+    const networkId = await web3.eth.net.getId()
+    const networkData = Token.networks[networkId]
 
-      {trailerUrl !== "" && <YouTube videoId={trailerUrl} opts={youtubeOpts} />}
-    </div>
-  )
+    if (networkData) {
+      const film = new web3.eth.Contract(Token.abi, networkData.address)
+      this.setState({ film })
+
+      const filmCount = await film.methods.filmsCount().call()
+
+      this.setState({ filmCount })
+
+      for (var i = 1; i <= filmCount; i++) {
+        const filmData = await film.methods.films(i).call()
+        const filmDetail = await film.methods.filmsDetails(i).call()
+
+        this.setState({ filmDetails: [...this.state.filmDetails, { ...filmData, ...filmDetail }] })
+      }
+
+      this.setState({ loading: false })
+
+    } else {
+      window.alert("Auction contract is not deployed to detected network")
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <center style={{ padding: '100px' }}>
+          <Spinner animation="border" size="lg" role="status">
+          </Spinner>
+        </center>
+      )
+    }
+
+    return (
+      <div className="row">
+        <h2>{this.props.title}</h2>
+        <br />
+        <ScrollContainer className="row__posters">
+          {this.state.filmDetails.map((filmDetail, index) => (
+            < img
+              // onClick={() =>
+              //   movieClicked(movie.name || movie.title || movie.orginal_name)
+              // }
+              key={index}
+              className={`row__poster ${this.props.isLargeRow && "row__posterLarge"}`}
+              src={`https://ipfs.infura.io/ipfs/${filmDetail.poster}`}
+              alt={'Request'}
+            />
+          ))}
+        </ScrollContainer>
+      </div>
+    )
+  }
 }
 
 
